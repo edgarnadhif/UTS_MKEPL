2ffc3f072e27109d7c4f58326a6a7dafd05a6fe3
edgarnadhif <edgarndf345@gmail.com>
2025-04-22 23:20:59 +0700

    refactoring: menyederhanakan constructor HotelRoomBooking menggunakan objek Guest dan BookingInfo

--- 08_Assessment_Tugas_Besar_Pengganti_UTS/src/model/HotelRoomBooking.java
+++ 08_Assessment_Tugas_Besar_Pengganti_UTS/src/model/HotelRoomBooking.java
@@ -1,26 +1,32 @@
-import java.util.Date;
 
 public class HotelRoomBooking {
+    private Guest guest;
+    private BookingInfo booking;
+
+    public HotelRoomBooking(Guest guest, BookingInfo booking) {
+        this.guest = guest;
+        this.booking = booking;
+    }
 
     
     public void cetakDetailPemesanan() {
         System.out.println("===== DETAIL PEMESANAN KAMAR =====");
-        // System.out.println("Nama Pemesan : " + namaPemesan);
-        // System.out.println("Jenis Kelamin: " + jenisKelamin);
-        // System.out.println("No. KTP      : " + nomorKTP);
-        // System.out.println("Telepon      : " + nomorTelepon);
-        // System.out.println("Email        : " + email);
-        System.out.println("Jenis Kamar  : " + jenisKamar);
-        System.out.println("Jumlah Tamu  : " + jumlahTamu);
-        System.out.println("Jumlah Malam : " + jumlahMalam);
-        System.out.println("Harga/Malam  : " + hargaPerMalam);
-        System.out.println("Check-in     : " + tanggalCheckin);
-        System.out.println("Check-out    : " + tanggalCheckout);
-        System.out.println("Status Aktif : " + statusAktif);
-        System.out.println("Voucher      : " + kodeVoucher);
-        System.out.println("Sudah Dibayar: " + sudahDibayar);
-        System.out.println("Total Biaya  : Rp " + hitungTotalBiaya());
-        System.out.println("Tipe Tamu    : " + klasifikasiTamu());
+        System.out.println("Nama Pemesan : " + guest.getNama());
+        System.out.println("Jenis Kelamin: " + guest.getJenisKelamin());
+        System.out.println("No. KTP      : " + guest.getNomorKTP());
+        System.out.println("Telepon      : " + guest.getNomorTelepon());
+        System.out.println("Email        : " + guest.getEmail());
+        System.out.println("Jenis Kamar  : " + booking.getJenisKamar());
+        System.out.println("Jumlah Tamu  : " + booking.getJumlahTamu());
+        System.out.println("Jumlah Malam : " + booking.getJumlahMalam());
+        System.out.println("Harga/Malam  : " + booking.getHargaPerMalam());
+        System.out.println("Check-in     : " + booking.getCheckin());
+        System.out.println("Check-out    : " + booking.getCheckout());
+        System.out.println("Status Aktif : " + booking.isStatusAktif());
+        System.out.println("Voucher      : " + booking.getKodeVoucher());
+        System.out.println("Sudah Dibayar: " + booking.isSudahDibayar());
+        System.out.println("Total Biaya  : Rp " + booking.hitungTotalBiaya());
+        System.out.println("Tipe Tamu    : " + booking.klasifikasiTamu());
         System.out.println("===================================");
     }
 
